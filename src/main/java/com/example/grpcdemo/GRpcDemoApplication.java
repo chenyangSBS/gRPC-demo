5c5afff658e91dd55db1f788f6abe3dfc7a2fb43
chenyangSBS <chenyangSBS@github.com>
2024-10-31 09:32:14 +0800

    first commit

---
+++ src/main/java/com/example/grpcdemo/GRpcDemoApplication.java
@@ -0,0 +1,10 @@
+package com.example.grpcdemo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GRpcDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GRpcDemoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/grpcdemo/GRpcProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grpcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GRpcProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
